4f046faaeb2077e1972a967079f5ac968e00222f
dulalaavas <144573527+dulalaavas@users.noreply.github.com>
2025-06-20 17:23:55 +0545

    Done Q1

---
+++ Q1.java
@@ -0,0 +1,51 @@
+abstract class Shape {
+    abstract double calculateArea();
+}
+
+class Circle extends Shape {
+    double radius;
+    Circle(double radius) { 
+        this.radius = radius; 
+    }
+
+    double calculateArea() {
+        return 3.14 * radius * radius;
+    }
+}
+
+class Rectangle extends Shape {
+    double length, width;
+    Rectangle(double l, double w) { 
+        length = l; 
+        width = w; 
+    }
+
+    double calculateArea() {
+        return length * width;
+    }
+}
+
+class Triangle extends Shape {
+    double base, height;
+    Triangle(double b, double h) { 
+        base = b; 
+        height = h; 
+    }
+
+    double calculateArea() {
+        return 0.5 * base * height;
+    }
+}
+
+public class Q1 {
+    public static void main(String[] args) {
+        Shape[] shapes = {
+            new Circle(5),
+            new Rectangle(4, 6),
+            new Triangle(3, 7)
+        };
+        for (Shape s : shapes) {
+            System.out.println("Area: " + s.calculateArea());
+        }
+    }
+}
